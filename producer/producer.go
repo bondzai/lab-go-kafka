b8c9d6b45be25a23df316cc581b0501323c9131a
JamesBond <introbond.dev@gmail.com>
2023-10-29 19:32:58 +0700

    fetch bitcoin price

--- producer/producer.go
+++ producer/producer.go
@@ -1,7 +1,9 @@
 package producer
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"os"
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
@@ -40,4 +42,35 @@ func ProduceMessage(topic, message string) error {
 	fmt.Println("Message sent:", message)
 	producer.Flush(15 * 1000) // Wait for any outstanding messages to be delivered and delivery reports to be received.
 	return nil
+}
+
+type CoinGeckoPriceResponse struct {
+	Bitcoin struct {
+		Usd float64 `json:"usd"`
+	} `json:"bitcoin"`
+}
+
+func FetchBitcoinPrice() (float64, error) {
+	coinGeckoURL := "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
+	resp, err := http.Get(coinGeckoURL)
+	if err != nil {
+		return 0, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return 0, fmt.Errorf("HTTP status %d", resp.StatusCode)
+	}
+
+	var priceResponse CoinGeckoPriceResponse
+	if err := json.NewDecoder(resp.Body).Decode(&priceResponse); err != nil {
+		return 0, err
+	}
+
+	return priceResponse.Bitcoin.Usd, nil
+}
+
+func ProduceBitcoinPriceMessage(topic string, priceUSD float64) error {
+	message := fmt.Sprintf("Bitcoin price in USD: %.2f", priceUSD)
+	return ProduceMessage(topic, message)
 }
