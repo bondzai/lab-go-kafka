c0dd4da2e7c52266de9546b1cba39f37b1a7d603
JamesBond <introbond.dev@gmail.com>
2023-11-18 08:35:03 +0700

    Refactor

--- frontend/cmd/web/main.go
+++ frontend/cmd/web/main.go
@@ -12,8 +12,9 @@ func main() {
 		render(w, "test.page.gohtml")
 	})
 
-	fmt.Println("Starting front end service on port 80")
-	err := http.ListenAndServe(":80", nil)
+	fmt.Println("Starting front end service on port 8084")
+	err := http.ListenAndServe(":8084", nil)
+
 	if err != nil {
 		log.Panic(err)
 	}
@@ -30,9 +31,7 @@ func render(w http.ResponseWriter, t string) {
 	var templateSlice []string
 	templateSlice = append(templateSlice, fmt.Sprintf("./cmd/web/templates/%s", t))
 
-	for _, x := range partials {
-		templateSlice = append(templateSlice, x)
-	}
+	templateSlice = append(templateSlice, partials...)
 
 	tmpl, err := template.ParseFiles(templateSlice...)
 	if err != nil {
